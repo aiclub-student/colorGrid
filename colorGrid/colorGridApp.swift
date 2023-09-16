08d0ac1a5d313779b6bc4ffbe7d124c8ef7623f8
Danika Gupta <danika@gprof.com>
2023-09-16 09:28:00 -0700

    Initial Commit

---
+++ colorGrid/colorGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorGridApp.swift
+//  colorGrid
+//
+//  Created by Amit Gupta on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct colorGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
